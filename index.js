de6ec1dfafb5d90a7dd0f8e40e3e8c3094f18532
zahraniass27 <zahra.niass27@compscihigh.org>
2025-10-23 16:39:52 +0000

    Added variable

--- index.js
+++ index.js
@@ -11,23 +11,28 @@
 // 1️⃣ String
 // Declare a variable named studentName and store your name as a string.
 // Use string interpolation to log the sentence describing its value and type.
-
+let studentName="Zahra"
+`${studentName} is a string.`
 // 2️⃣ Number
 // Declare a variable named studentAge and store your age as a number.
 // Use string interpolation to log the sentence describing its value and type.
-
+let studentAge=16
+ console.log(`${studentAge} is a Number`)
 // 3️⃣ Boolean
 // Declare a variable named isEnrolled and assign it either true or false.
 // Use string interpolation to log the sentence describing its value and type.
-
+let isEnrolled= true
+ console.log(`${isEnrolled} is a boolean`)
 // 4️⃣ Undefined
 // Declare a variable named favoriteSubject but do not assign it a value.
 // Use string interpolation to log the sentence describing its value and type.
-
+let favoriteSubject= 
+ console.log(`${favoriteSubject} is undefined`)
 // 5️⃣ Null
 // Declare a variable named nextClass and set it equal to null.
 // Use string interpolation to log the sentence describing its value and type.
-
+let nextClass= null
+ console.log(`${nextClass} is a null`)
 // 6️⃣ Check
 // When finished, run your file to verify that all outputs print correctly.
 // Run your file by typing node index.js in the terminal
